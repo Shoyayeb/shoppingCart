2a354c3132cacd335ef59054728ac9e1c65ed334
Shafin <sa.fine@outlook.com>
2021-08-19 10:38:34 +0600

    function update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function updateCaseNumber(isIncreasing) {
-    const caseInput = document.getElementById("case-number");
+function updateCaseNumber(product, price, isIncreasing) {
+    const caseInput = document.getElementById(product + "-number");
     let caseNumber = caseInput.value;
     if (isIncreasing == true) {
         caseNumber = parseInt(caseNumber) + 1;
@@ -7,30 +7,19 @@ function updateCaseNumber(isIncreasing) {
         caseNumber = parseInt(caseNumber) - 1;
     }
     caseInput.value = caseNumber;
-    const caseTotal = document.getElementById("case-total");
-    caseTotal.innerText = caseNumber * 59;
-}
-function updatePhoneNumber(isIncreasing) {
-    const phoneInput = document.getElementById("phone-number");
-    let phoneNumber = phoneInput.value;
-    if (isIncreasing == true) {
-        phoneNumber = parseInt(phoneNumber) + 1;
-    } else if (phoneNumber > 0) {
-        phoneNumber = parseInt(phoneNumber) - 1;
-    }
-    phoneInput.value = phoneNumber;
-    const PhoneTotal = document.getElementById("phone-total");
-    PhoneTotal.innerText = phoneNumber * 1219;
+    const caseTotal = document.getElementById(product + "-total");
+    caseTotal.innerText = caseNumber * price;
 }
 document.getElementById("phone-plus").addEventListener("click", function () {
-    updatePhoneNumber(true);
-})
+    updateCaseNumber("phone", 1219, true);
+});
 document.getElementById("phone-minus").addEventListener("click", function () {
-    updatePhoneNumber(false);
-})
+    updateCaseNumber("phone", 1219, false);
+});
+// 
 document.getElementById("case-plus").addEventListener("click", function () {
-    updateCaseNumber(true);
-})
+    updateCaseNumber("case", 59, true);
+});
 document.getElementById("case-minus").addEventListener("click", function () {
-    updateCaseNumber(false);
-})
+    updateCaseNumber("case", 59, false);
+});
